682c5604a2cbbe1347fd60923a1843373b6b4dc6
Charles <charlessssoares00@gmail.com>
2024-10-22 15:36:00 -0300

    :tada: Initial commit

---
+++ src/main/java/br/com/scrumvoter/ScrumVoterApplication.java
@@ -0,0 +1,13 @@
+package br.com.scrumvoter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumVoterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumVoterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/scrumvoter/ScrumVoterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.scrumvoter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumVoterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
